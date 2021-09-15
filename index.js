f156d389ca3f8ccb3120f2bc372e9f0c084d4bac
Pavel Kopychenko <t34box@gmail.com>
2021-09-15 20:25:48 +0300

    New functions: sefFlow... and getFlow... .

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-const constant = v => () => v
+const isNil = (value) => value == null
+const isBoolean = (value) => value === true || value === false
+const isNumber = (value) => typeof value == 'number'
+const isString = (value) => typeof value == 'string'
+const isArray = Array.isArray
+const isObject = (value) => {
+    const type = typeof value
+    return value != null && (type == 'object' || type == 'function')
+}
+
+const constant = (v) => () => v
 
 const flow = (...fns) => {
-  const f = fns.pop()
-  return f ? v => f(flow(...fns)(v)) : v => v
+    const f = fns.pop()
+    return f ? (v) => f(flow(...fns)(v)) : (v) => v
 }
 
 const thread = (arg, ...fns) => flow(...fns)(arg)
 
+const getFlow = (path, defaultValue) =>
+    flow((o) => o || defaultValue, ...path.map((k) => (o) => isNil(o) ? o : o[k]))
+
+const get = (o, path, defaultValue) => getFlow(path, defaultValue)(o)
+
+const setFlow = (path, value) => (o) => {
+    const setByKey = (o, [k, ...keys], value) => {
+        if ((k && isObject(o)) || isArray(o)) {
+            o[k] = setByKey(o[k] || {}, keys, value)
+            return o
+        } else return value
+    }
+    return setByKey(o, path, value)
+}
+
+const set = (o, path, value) => setFlow(path, value)(o)
+
 module.exports = {
-  constant,
-  flow,
-  thread
+    constant,
+    flow,
+    thread,
+    isNil,
+    isBoolean,
+    isNumber,
+    isString,
+    isArray,
+    isObject,
+    getFlow,
+    get,
+    setFlow,
+    set
 }
--- test/test.js
+++ test/test.js
@@ -2,34 +2,85 @@
  * @jest-environment node
  */
 import { jest, test, expect } from '@jest/globals'
-import { constant, flow } from '../index'
+import { constant, flow, getFlow, get, setFlow, set } from '../index'
 
 jest.setTimeout(30000)
 
 test('test constant', () => {
-  expect(constant(10)()).toBe(10)
+    expect(constant(10)()).toBe(10)
 })
 
 test('test flow', () => {
-  const inc = i => i + 1
-  const dec = i => i - 1
-  const square = i => i * i
+    const inc = (i) => i + 1
+    const dec = (i) => i - 1
+    const square = (i) => i * i
 
-  const primer1Flow = flow(inc, inc, dec, inc, inc, square, o => `result ${o}`)
+    const primer1F = flow(inc, inc, dec, inc, inc, square, (o) => `result ${o}`)
 
-  const result = primer1Flow(1)
+    const result = primer1F(1)
 
-  expect(result).toBe('result 16')
+    expect(result).toBe('result 16')
 })
 
 test('test thread', () => {
-  const inc = i => i + 1
-  const dec = i => i - 1
-  const square = i => i * i
+    const inc = (i) => i + 1
+    const dec = (i) => i - 1
+    const square = (i) => i * i
 
-  const primer1Flow = flow(inc, inc, dec, inc, inc, square, o => `result ${o}`)
+    const primer1F = flow(inc, inc, dec, inc, inc, square, (o) => `result ${o}`)
 
-  const result = primer1Flow(1)
+    const result = primer1F(1)
 
-  expect(result).toBe('result 16')
+    expect(result).toBe('result 16')
+})
+
+test('test getFlow 1', () => {
+    {
+        const obj = { a: 1, b: { c: 1 } }
+        const path = ['b', 'c']
+        const defaultValue = 222
+        const getFlow1 = getFlow(path, defaultValue)
+
+        expect(getFlow1(obj)).toBe(1)
+        expect(get(obj, path, defaultValue)).toBe(1)
+    }
+})
+
+test('test getFlow 2', () => {
+    {
+        const obj = { a: 1, b: { c: [{ d: 1 }] } }
+        const path = ['b', 'c', 0, 'd']
+        const defaultValue = 222
+        const getFlow1 = getFlow(path, defaultValue)
+
+        expect(getFlow1(obj)).toBe(1)
+        expect(get(obj, path, defaultValue)).toBe(1)
+    }
+})
+
+test('test setFlow 1', () => {
+    {
+        const obj = { a: 1, b: { c: { d: 1 } } }
+        const path = ['b', 'c', 'd']
+        const value = 222
+        const setFlow1 = setFlow(path, value)
+
+        setFlow1(obj)
+        expect(obj.b.c.d).toBe(value)
+
+        set(obj, [1, 2, 3, 4, 5], 6)
+        expect(obj['1']['2']['3']['4']['5']).toBe(6)
+    }
+})
+
+test('test setFlow 2', () => {
+    {
+        const obj = { a: 1, b: { c: [{ d: 1 }] } }
+        const path = ['b', 'c', 0, 'd']
+        const value = 222
+        const setFlow1 = setFlow(path, value)
+
+        setFlow1(obj)
+        expect(obj.b.c[0].d).toBe(value)
+    }
 })
