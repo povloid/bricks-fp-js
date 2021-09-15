c97d98b0e4a4d0835d037080e3a08477519e0988
Pavel Kopychenko <t34box@gmail.com>
2021-09-15 16:49:33 +0300

    Convert code to es6.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-export const constant = v => () => v
-export const flow = (...args) => {
+const constant = v => () => v
+const flow = (...args) => {
   const f = args.pop()
   if (f) return v => f(flow(...args)(v))
   else return v => v
+}
+
+module.exports = {
+  constant,
+  flow
 }
